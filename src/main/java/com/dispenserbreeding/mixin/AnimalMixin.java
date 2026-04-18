96d0c486117ed801573538063778ad1e4e6582f7
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2026-04-18 23:27:48 +1000

    Use Mob accessor for goalSelector in AnimalMixin
    Replace the direct @Shadow access to the animal goalSelector with a generated accessor. Adds a new MobAccessor mixin that exposes Mob.goalSelector via an @Accessor method, updates AnimalMixin to cast the Animal to Mob and obtain the GoalSelector through the accessor, and registers the new MobAccessor in dispenserbreeding.mixins.json. This removes the @Shadow field from AnimalMixin and centralizes access to the GoalSelector for adding the GroundBreedingItemGoal.

--- src/main/java/com/dispenserbreeding/mixin/AnimalMixin.java
+++ src/main/java/com/dispenserbreeding/mixin/AnimalMixin.java
@@ -1,10 +1,10 @@
 package com.dispenserbreeding.mixin;
 
 import com.dispenserbreeding.breeding.GroundBreedingItemGoal;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.animal.Animal;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,9 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Animal.class)
 public abstract class AnimalMixin {
-	@Shadow
-	protected GoalSelector goalSelector;
-
 	@Unique
 	private boolean dispenserbreeding$groundGoalAdded = false;
 
@@ -25,6 +22,7 @@ public abstract class AnimalMixin {
 		}
 
 		Animal self = (Animal) (Object) this;
+		GoalSelector goalSelector = ((MobAccessor) (Mob) self).dispenserbreeding$getGoalSelector();
 		goalSelector.addGoal(5, new GroundBreedingItemGoal(self));
 		dispenserbreeding$groundGoalAdded = true;
 	}
